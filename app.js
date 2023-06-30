98d26dacf06e1d474d3e983593c4c0154debb69c
theamik <amik.sylhet@gmail.com>
2023-06-30 23:19:28 +0600

    initail commit 5

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use("/api/v1/", other);
 export default app;
 
 app.get("/", (req, res) => {
-    res.send(`<h1>Site is working. click <a href=${process.env.FRONTEND_URL}></a> to visit frontend.</h1>`);
+    res.send(`<h1>Site is working. click <a href=${process.env.FRONTEND_URL}>here....</a> to visit frontend.</h1>`)
 });
 
 
