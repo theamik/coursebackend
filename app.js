d2a9b213b8d422b436489168187a5db0306de3aa
theamik <amik.sylhet@gmail.com>
2023-06-30 23:11:16 +0600

    initail commit 4

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { config } from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 
 config({
@@ -12,8 +13,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true,
-})); 
+}));
 app.use(cookieParser());
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"]
+}))
 
 import course from './routes/courseRoutes.js';
 import user from './routes/userRoutes.js';
@@ -27,5 +33,12 @@ app.use("/api/v1/", payment);
 app.use("/api/v1/", other);
 
 export default app;
+
+app.get("/", (req, res) => {
+    res.send(`<h1>Site is working. click <a href=${process.env.FRONTEND_URL}></a> to visit frontend.</h1>`);
+});
+
+
+
 
 app.use(ErrorMiddleware)
