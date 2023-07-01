fec7afa5edda880321d5ea984e5645127fab5d39
theamik <amik.sylhet@gmail.com>
2023-07-01 16:33:13 +0600

    initail commit 7

--- controllers/userController.js
+++ controllers/userController.js
@@ -55,6 +55,9 @@ export const login = catchAsyncError(async (req, res, next) => {
 export const logout = catchAsyncError(async (req, res, next) => {
     res.status(200).cookie("token", null, {
         expires: new Date(Date.now()),
+        httpOnly:true,
+        secure:true,
+        sameSite:"none",
     }).json({
         success: true,
         message: "Successfully Logout !",
