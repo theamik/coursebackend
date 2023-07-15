9dead596673d42f73fa8aeeeb876e039a0164085
theamik <amik.sylhet@gmail.com>
2023-07-15 11:27:41 +0600

    initail commit 16

--- routes/otherRoutes.js
+++ routes/otherRoutes.js
@@ -1,13 +1,13 @@
 import express from "express";
 
 import { authorizedAdmin, isAuthenticated } from "../middlewares/auth.js";
-import {  contact, courseRequest, getDashboardStat } from "../controllers/otherController.js";
+import {  contact, courseRequest, getDashboardStats } from "../controllers/otherController.js";
 const router = express.Router();
 
 router.route("/contact").post(contact);
 
 router.route("/courserequest").post(courseRequest);
 
-router.route("/admin/stats").get(isAuthenticated, authorizedAdmin, getDashboardStat);
+router.route("/admin/stats").get(isAuthenticated, authorizedAdmin, getDashboardStats);
 
 export default router;
